052a2831a469d65e1be3cd63f33992446ff65c78
zhasmin.s <shonazmievajasmin@gmail.com>
2023-03-19 09:45:33 +0500

    do not show the city

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c1aca6ac26msh8f8a07ce47d48eep1194e2jsne5c477c0be86',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6eb9a854bffea16ae403ddebe2671f0b";
